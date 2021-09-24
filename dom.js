7cfb98da8018da527bcfa2621c62542376e8ec7e
vFrank <vfrankpzhang@gmail.com>
2021-09-24 19:26:06 +0800

    update

--- dom.js
+++ dom.js
@@ -1,13 +1,29 @@
-window.dom = {
-    find(){
-        你来补全
+window.dom={
+    find(selector,scope) {
+        return (scope ||document).querySelectorAll(selector)
     },
-    style(){
-        你来补全
+    style(node,name,value){
+        if(arguments.length===3){
+            node.style[name]=value
+        }else if(arguments.length ===2){
+            if(typeof name === 'string'){
+            return node.style[name]
+            }else if(name instanceof Object){
+                const object =name
+                for(let key in object){
+                    node.style[key] =object[key]
+        
+                }
+            
+            }
+      
+        }
     },
-    each(){
-        你来补全
-    }
+    each(nodeList,fn){
+        for(let i=0;i<nodeList.length;i++){
+            fn.call (null,nodeList[i])
+        }
+    },
 }
 
 const div = dom.find('#test>.red')[0] // 获取对应的元素
